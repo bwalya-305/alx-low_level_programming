19e3526f6300f624221c86b8711d630cb831d201
bwalyamwa <bwalyamwansa305@gmail.com>
2022-09-13 13:50:48 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints in lowercase
+ */
+void print_alphabet_x10(void);
+{
+	int j;
+	int k;
+
+	for (k = 0; j < 10; k++)
+	{
+		for (j = 'a'; k <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar(('\n');
+	}
+}
