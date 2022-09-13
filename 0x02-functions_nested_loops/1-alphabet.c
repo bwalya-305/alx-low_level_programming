28ab56d20d31fb9258e8900784e19056487477ca
bwalyamwa <bwalyamwansa305@gmail.com>
2022-09-13 08:14:36 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the english alphabet in lower case.
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
