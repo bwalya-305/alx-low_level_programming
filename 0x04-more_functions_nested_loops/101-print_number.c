efe09da414849cf61cd32297fb0455a6bc073d5c
bwalyamwa <bwalyamwansa305@gmail.com>
2022-09-16 02:34:16 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,18 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int x;
+
+	x = n'
 
 	if (n < 0)
 	{
-		putchar('-');
-		num = -num;
+		putchar(45);
+		x = -n
 	}
-	if (num > 9)
+	if (x / 10)
 	{
-		print_number(num / 10);
+		print_number(x / 10);
 	}
-	putchar(num % 10 + '0');
+	putchar(x % 10 + '0');
 }
