1fa1d59a1186622690adb324f39e21559419df04
bwalyamwa <bwalyamwansa305@gmail.com>
2023-01-08 22:26:16 +0200

    4. >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,29 +4,28 @@
 #include "hash_tables.h"
 
 /**
-  * hash_table_get - Retrieves a value associated with a key
-  * @ht: The hash table to look into
-  * @key: The key to finding
-  *
-  * Return: The value associated with the element
-  * or NULL if key couldn't be found
-  */
+ * hash_table_get - returns that value of the key
+ * @ht: hash table to search in.
+ * @key: key to find
+ *
+ * Return: Value at the key location
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int idx = 0;
-	hash_node_t *elem = NULL;
+	unsigned long int idx;
+	hash_node_t *node;
 
-	if (ht == NULL || key == NULL)
+	if (!ht || !key)
 		return (NULL);
+	idx = key_index((unsigned char *)key, ht->size);
+	node = ht->array[idx];
 
-	idx = key_index((unsigned char *) key, ht->size);
-	elem = ht->array[idx];
+	while (node)
+	{
+		if (!strcmp(key, node->key))
+			return (node->value);
+		node = node->next;
+	}
 
-	if (elem == NULL)
-		return (NULL);
-
-	while (strcmp(key, elem->key) != 0)
-		elem = elem->next;
-
-	return (elem->value);
+	return (NULL);
 }
