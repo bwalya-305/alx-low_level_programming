7755d68abef0ae6c80bb078233c2b8801b932c62
bwalya-305 <bwalyamwansa305@gmail.com>
2023-07-11 13:10:05 -0700

    added more prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,6 +14,10 @@ int advanced_binary(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
+void print_list(const listint_t *list);
+void free_list(listint_t *list);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
 
 #endif
